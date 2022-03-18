c7adb1d6f3d8a250300dcaf754901812688d100c
SteveOtieno <stevemenya14@gmail.com>
2022-03-17 20:15:12 -0500

    moving files to correct directory

--- Intro_To_Programming/Chapter1Example/src/Credits.java
+++ Intro_To_Programming/Chapter1Example/src/Credits.java
@@ -6,22 +6,33 @@ import java.util.Scanner;
 public class Credits {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		//Initialize Scanner object
-		Scanner sc = new Scanner(System.in);
-		
-		//Create variable to hold the integer response from student
-		int credits;
-		
-		//ask student via prompt
-		System.out.println("Enter the number of credits");
-		credits = sc.nextInt();
-		
-		//Display students response
-		System.out.println("If taking " + credits + ", " + credits * 2 + " study hours are suggested");
-
-		
-	}
-
+	      Scanner scnr = new Scanner(System.in);
+	      int num1;
+	      int num2;
+	      int num3;
+	      int num4;
+	      int product;
+	      int sum;
+	      int average;
+	      double prod;
+	      double avg;
+	      
+	      
+	      /* Type your code here. */
+	      num1 = scnr.nextInt();
+	      num2 = scnr.nextInt();
+	      num3 = scnr.nextInt();
+	      num4 = scnr.nextInt();
+	      
+	      product = num1 * num2 * num3 * num4;		    
+	      sum = num1 + num2 + num3 + num4;		      
+	      average = sum / 4;
+	      System.out.println(product + " " + average);
+	      
+	      prod = (double)num1 * num2 * num3 * num4; 
+	      avg = (num1 * num2 * num3 * num4)/4.0;
+	      
+	      System.out.printf("%.3f %.3f\n", prod, avg);
+	      
+	   }
 }
