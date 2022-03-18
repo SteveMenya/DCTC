6c4ca2d40228644074ff2f0fa7d83c517730874d
SteveOtieno <stevemenya14@gmail.com>
2022-03-17 20:50:42 -0500

    chapter 8

---
+++ DotNet/Chapter8/BarChart/BarChart/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarChart
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 0, 2, 0, 0, 0, 0, 1, 2, 4, 2, 1 };
+            Console.WriteLine("Grade Distribution");
+
+            //for each array element, output a bar of the chart
+
+            for(var counter = 0 ; counter < array.Length; counter++)
+            {
+                //output bar labels("00-09: "..."90-99: ", "100: ")
+                if(counter == 10)
+                {
+                    Console.Write("100:");
+                }
+                else
+                {
+                    Console.Write($"{counter * 10:D2} - {counter * 10 + 9:D2}: ");
+
+                }
+
+                //display bar of asterisks
+                for (var stars =0; stars < array[counter]; ++ stars)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ DotNet/Chapter8/Dice_Rolling/Dice_Rolling/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dice_Rolling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //JJ Menya
+            //Chapter 8 hw
+
+            //introduce helper class
+            RollDice rl = new RollDice();
+
+            //roll dice 36000 times
+            rl.rollDice(36000);
+        }
+    }
+}
