1acd4056a9ab1d875eb35dcca30db1e9e5f8744a
SteveOtieno <stevemenya14@gmail.com>
2022-03-17 21:00:30 -0500

    chapter 5

---
+++ DotNet/Chapter5/Credit_Limit_Calculator/Credit_Limit_Calculator/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Credit_Limit_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //JJ Menya
+            //Exercise 5.18
+
+
+            //variables
+            int accoutNu = 0;
+            double monthBalance = 0;
+            double itemT = 0;
+            double totalC = 0;
+            int allowedC = 0;
+            double newBal;
+
+            //step 1: Set up the sentinel_value
+            Console.WriteLine("Enter account number or -1 to quit: ");
+            accoutNu = Int32.Parse(Console.ReadLine());
+            while (accoutNu != -1)
+            {
+                //Step 2: Prompt cutomer to insert account number
+                Console.WriteLine("What is your balance at the beginning of the month?");
+                monthBalance = double.Parse(Console.ReadLine());
+                if(monthBalance < 0)
+                {
+                    Console.WriteLine("Monthly Balance must be 0 or greater\nWhat is your balance at the beginning of the month?");
+                    monthBalance = Convert.ToDouble(Console.ReadLine());
+                }
+
+                //Step 3: Prompt cutomer to insert total of all items
+                Console.WriteLine("What is the total of all items charged?");
+                itemT = Convert.ToDouble(Console.ReadLine());
+                if (itemT < 0)
+                {
+                    Console.WriteLine("Total of all items charged must be 0 or greater\nWhat is the total of all items charged?");
+                    monthBalance = Convert.ToDouble(Console.ReadLine());
+                }
+
+                //Step 4: Prompt cutomer to insert total of all items
+                Console.WriteLine("What is the total of all credits charged?");
+                totalC = Convert.ToDouble(Console.ReadLine());
+                if (totalC < 0)
+                {
+                    Console.WriteLine("Total of all credits charged must be 0 or greater\nWhat is the total of all credits charged?");
+                    monthBalance = Convert.ToDouble(Console.ReadLine());
+                }
+
+                //Step 5: Prompt cutomer to insert total of all items
+                Console.WriteLine("What is the allowed credit limit?");
+                allowedC = Int32.Parse(Console.ReadLine());
+                if (allowedC < 0)
+                {
+                    Console.WriteLine("Allowed Credit Limit must be 0 or greater\nWhat is the allowed credit limit?");
+                    monthBalance = Convert.ToDouble(Console.ReadLine());
+                }
+
+                Console.WriteLine("Enter account number or -1 to quit: ");
+                accoutNu = Int32.Parse(Console.ReadLine());
+
+                if (accoutNu != 0)
+                {
+                    newBal = (monthBalance + itemT + totalC) - allowedC;
+                    Console.WriteLine("The New Balance is " + newBal);
+                    Console.ReadLine();
+
+                    if (newBal > allowedC)
+                    {
+                        double diff = newBal - allowedC;
+                        Console.WriteLine("Credit Limit Exceeded by: " + diff);
+                        Console.ReadLine();
+                    }
+                }
+
+                else
+                {
+                    Console.WriteLine("No data  were entered");
+                    Console.ReadLine();
+                }
+
+            }
+        }
+    }
+}
---
+++ DotNet/Chapter5/Credit_Limit_Calculator/Credit_Limit_Calculator/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Credit_Limit_Calculator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("Credit_Limit_Calculator")]
+[assembly: AssemblyCopyright("Copyright © HP 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cab0a828-fdfb-47c7-a5ad-a459d55b9bcf")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
