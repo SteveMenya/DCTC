a48b12842558d04a19db33079b503bfe1146e294
Jashon Menya <jjmenya14@gmail.com>
2022-10-10 13:19:44 -0800

    components and props

--- Cross_Platform_Web_Dev/expenses-app/src/components/ExpenseItems.js
+++ Cross_Platform_Web_Dev/expenses-app/src/components/ExpenseItems.js
@@ -1,11 +1,13 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItems.css';
 
 
 function ExpenseItems(props) {
 
+
     return (
         <div className="expense-item">
-            <div>{props.date.toISOString()} </div>
+            <ExpenseDate date={props.date} />
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
